efae5149c9e30dca6bcc82260bdcc4a1ba717b57
mykhaylo zhovkevych <mzovkevic@gmail.com>
2026-09-10 17:40:58 +0200

    added the item UI

--- multitenant-ecommerce/src/app/modules/products/server/procedures.ts
+++ multitenant-ecommerce/src/app/modules/products/server/procedures.ts
@@ -5,6 +5,7 @@ import {baseProcedure, createTRPCRouter} from "@/trpc/init";
 import {sortValues} from "@/app/modules/products/search-params";
 import {Media, Tenant} from "@/payload-types";
 import {DEFAULT_LIMIT} from "@/constants/constants";
+import {PluginMultiTenantTranslationKeys} from "@payloadcms/plugin-multi-tenant/translations/languages/all";
 
 export const productsRouter = createTRPCRouter({
     getOne: baseProcedure
@@ -16,12 +17,14 @@ export const productsRouter = createTRPCRouter({
         .query(async ({ ctx, input }) => {
             const product = await  ctx.payload.findByID({
                 collection: "products",
-                id: input.id
+                id: input.id,
+                depth: 2,
             });
 
             return {
                 ...product,
-                image: product.image as Media | null
+                image: product.image as Media | null,
+                tenant: product.tenant as Tenant & { image: Media | null },
             }
         }),
     getMany: baseProcedure
